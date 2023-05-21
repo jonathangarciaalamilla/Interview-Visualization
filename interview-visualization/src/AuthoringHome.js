b56d1415a46eafea4f1cdd1a91ef6d082347766f
Jonathan Garcia <garciajonathan2000@gmail.com>
2023-05-20 23:24:23 -0500

    centering menu page

--- interview-visualization/src/AuthoringHome.js
+++ interview-visualization/src/AuthoringHome.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-//import AuthoringTool from "./AuthoringTool"
 import { DataGrid } from "@mui/x-data-grid";
-import logo from "./cirvr.png"; // Tell webpack this JS file uses this image
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Button, IconButton } from "@mui/material";
+import logo from "./cirvr.png";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { Grid, Item, TextField } from "@mui/material";
 import AuthoringTool from "./AuthoringTool";
@@ -22,27 +23,32 @@ const AuthoringHome = () => {
   function navigateToPage() {
     navigate("/tool", { state: "from-template" });
   }
+
   function navigateNew() {
     navigate("/tool", { state: "" });
   }
 
-  //We are going to create an interview list here and then navigate to <Authoring tool page?
-  //We will pass the original
   return (
-    <div style={{ height: 400, width: "100%", alignItems: "center" }}>
-      <img src={logo} alt="CIRVR logo" style={{ width: "20%" }}></img>
-      <h1 style={{ fontFamily: "Arial, Helvetica, sans-serif" }}>
-        {" "}
-        Interview Authoring Tool
-      </h1>
-      <Button color={"primary"} variant="constrained" onClick={navigateToPage}>
-        Edit Current Script
-      </Button>
+    <div className="center-container">
+      <div style={{ height: 400, width: "100%", alignItems: "center" }}>
+        <img src={logo} alt="CIRVR logo" style={{ width: "20%" }} />
+        <h1 style={{ fontFamily: "Arial, Helvetica, sans-serif" }}>
+          Interview Authoring Tool
+        </h1>
+        <Button
+          color={"primary"}
+          variant="constrained"
+          onClick={navigateToPage}
+        >
+          Edit Current Script
+        </Button>
 
-      <Button color={"primary"} variant="constrained" onClick={navigateNew}>
-        Create New Script
-      </Button>
+        <Button color={"primary"} variant="constrained" onClick={navigateNew}>
+          Create New Script
+        </Button>
+      </div>
     </div>
   );
 };
+
 export default AuthoringHome;
